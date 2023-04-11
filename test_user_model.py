193b9a5d6c1e1ecf2ed8b3e9a11146c50270a8d9
John Wood <johna613@gmail.com>
2023-04-11 08:01:23 -0400

    added user model tests

--- test_user_model.py
+++ test_user_model.py
@@ -7,6 +7,7 @@
 
 import os
 from unittest import TestCase
+from sqlalchemy import exc
 
 from models import db, User, Message, Follows
 
@@ -34,16 +35,37 @@ class UserModelTestCase(TestCase):
 
     def setUp(self):
         """Create test client, add sample data."""
+        db.drop_all()
+        db.create_all()
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        user1 = User.signup("user1", "email1@abc.com", "password", None, None)
+        uid1 = 1111
+        user1.id = uid1
+
+        user2 = User.signup("user2", "email2@abc.com", "password", None, None)
+        uid2 = 2222
+        user2.id = uid2
+
+        db.session.commit()
+
+        user1 = User.query.get(uid1)
+        user2 = User.query.get(uid2)
+
+        self.user1 = user1
+        self.uid1 = uid1
+
+        self.user2 = user2
+        self.uid2 = uid2
 
         self.client = app.test_client()
 
+    def tearDown(self):
+        db.session.rollback()
+        return super().tearDown()
+
     def test_user_model(self):
         """Does basic model work?"""
-
+      
         u = User(
             email="test@test.com",
             username="testuser",
@@ -56,3 +78,90 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+################################
+# following tests
+################################
+
+
+    def test_follows(self):
+        self.user1.following.append(self.user2)
+        db.session.commit()
+
+        self.assertEqual(len(self.user2.following), 0)
+        self.assertEqual(len(self.user2.followers), 1)
+        self.assertEqual(len(self.user1.followers), 0)
+        self.assertEqual(len(self.user1.following), 1)
+
+        self.assertEqual(self.user2.followers[0].id, self.user1.id)
+        self.assertEqual(self.user1.following[0].id, self.user2.id)
+
+    def test_is_following(self):
+        self.user1.following.append(self.user2)
+        db.session.commit()
+
+        self.assertTrue(self.user1.is_following(self.user2))
+        self.assertFalse(self.user2.is_following(self.user1))
+
+    def test_is_followed_by(self):
+        self.user1.following.append(self.user2)
+        db.session.commit()
+
+        self.assertTrue(self.user2.is_followed_by(self.user1))
+        self.assertFalse(self.user1.is_followed_by(self.user2))
+
+    ###########################
+    #Signup Tests
+    ###########################
+    def test_valid_signup(self):
+        u_test = User.signup("testtest", "test@test.com", "password", None, None)
+        uid = 99999
+        u_test.id = uid
+        db.session.commit()
+
+        u_test = User.query.get(uid)
+        self.assertIsNotNone(u_test)
+        self.assertEqual(u_test.username, "testtest")
+        self.assertEqual(u_test.email, "test@test.com")
+        self.assertNotEqual(u_test.password, "password")
+        self.assertTrue(u_test.password.startswith("$2b$"))
+
+    def test_invalid_username_signup(self):
+        invalid = User.signup(None, "test@test.com", "password", None, None)
+        uid = 12345
+        invalid.id = uid
+
+        #### I don't fully understand the use of the with keyword
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
+
+    def test_invalid_username_signup(self):
+        invalid = User.signup("testing", None, "password", None, None)
+        uid = 12345
+        invalid.id = uid
+
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
+    
+    def test_invalid_password_signup(self):
+        with self.assertRaises(ValueError) as context:
+            User.signup("testtest", "email@gmail.com", "", None, None)
+        ##### Why is the context different than above?
+
+        with self.assertRaises(ValueError) as context:
+            User.signup("testtest", "email@gmail.com", None, None, None)
+
+    ########
+    #Authentication Tests
+    ########
+
+    def test_valid_authentication(self):
+        u = User.authenticate(self.user1.username, "password")
+        self.assertIsNotNone(u)
+        self.assertEqual(u.id, self.uid1)
+
+    def test_invalid_username(self):
+        self.assertFalse(User.authenticate("badname", "password"))
+
+    def test_wrong_password(self):
+        self.assertFalse(User.authenticate(self.user1.username, "badpassword"))
